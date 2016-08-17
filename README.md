7e4eb9bb18ae9118ec2ea3375911b353ab126e0d
kebin <kekobin@163.com>
2016-08-17 13:07:28 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# gulp-md5
-gulp-md5 width gulp-md5-plus
+原理(以src/js/*.js为例)
+1.gulp.src('./src/*.js')获取所有的js
+2.pipe(md5(10,'./output/*.html')),这里将所有的js通过md5(),在md5中会将js一个个的生成对应的md5值,并且暂存其name,
+然后,根据传进去的参数截取md5值、读取目的文件并根据name搜索出要替换的文件名使用name_md5替换,然后再重新将该文件写回去
+
--- gulpfile.js
+++ gulpfile.js
@@ -35,10 +35,3 @@ gulp.task('img' , ['css'],function() {
 gulp.task('default',['clean'],function(){
 	gulp.start('html','css','img', 'js');
 })
-
-/**
-**步骤(以src/js/*.js为例)
-**1.gulp.src('./src/*.js')获取所有的js
-**2.pipe(md5(10,'./output/*.html')),这里将所有的js通过md5(),在md5中会将js一个个的生成对应的md5值,并且暂存其name,
-**  然后,根据传进去的参数截取md5值、读取目的文件并根据name搜索出要替换的文件名使用name_md5替换,然后在重新将该文件写回去
-**/
